30d65e8ba8ae00878139393d150ea90b4a8f1521
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2021-09-20 16:12:27 -0700

    trying mux-video element

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@mux/mux-video'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1>Hello world!</h1>
+    <mux-video
+      playback-id="DS00Spx1CV902MCtPj5WknGlR102V5HFkDe?foo=bar&biz=baz"
+      env-key="mux-data-env-key"
+      metadata-video-title="Big Buck Bunny"
+      metadata-viewer-user-id="user-id-1234"
+      controls
+    ></mux-video>
+  </div>
 </template>
 
 <script lang="ts">
